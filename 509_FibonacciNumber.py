8ba1d1c73d0711b7a2f99a6aa0fa6f90c5e5e112
seawavve <66352658+seawavve@users.noreply.github.com>
2020-11-01 19:52:49 +0900

    Update 509_FibonacciNumber.py

--- 509_FibonacciNumber.py
+++ 509_FibonacciNumber.py
@@ -1,3 +1,4 @@
+#DP
 class Solution:
     def fib(self, N: int) -> int:
         l=[None]*31
@@ -7,3 +8,10 @@ class Solution:
             l[i]=l[i-1]+l[i-2]
         return l[N]
             
+
+#recursion
+class Solution:
+    def fib(self, N: int) -> int:
+        if N==0: return 0
+        if N==1: return 1
+        return self.fib(N-1)+self.fib(N-2)
